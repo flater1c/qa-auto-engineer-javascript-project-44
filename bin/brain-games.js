97e1408a1888e6ad268cad15599d253f4f6c8d1d
flater1c <92336292+flater1c@users.noreply.github.com>
2024-10-08 17:14:57 +0200

    moving greeting to a function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import greeting from './cli.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}`);
+greeting();
