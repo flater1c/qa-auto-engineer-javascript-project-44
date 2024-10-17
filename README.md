6c453f861ebd823b7b1d222872cf2046d162cd1a
flater1c <92336292+flater1c@users.noreply.github.com>
2024-10-17 15:25:34 +0200

    gcd game added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/flater1c/qa-auto-engineer-javascript-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/flater1c/qa-auto-engineer-javascript-project-44/actions) [![Maintainability](https://api.codeclimate.com/v1/badges/29e6d92ad2520740ea63/maintainability)](https://codeclimate.com/github/flater1c/qa-auto-engineer-javascript-project-44/maintainability)
 
-<a href="https://asciinema.org/a/0Ads5U1nO3B2RIkwOH9CnxTQu">Brain even asciinema</a>
-<a href="https://asciinema.org/a/j1wyAIQ3mPAZbFji0vGcFo2Lp">Brain calc asciinema</a>
+<a href="https://asciinema.org/a/0Ads5U1nO3B2RIkwOH9CnxTQu">Brain even asciinema</a><br>
+<a href="https://asciinema.org/a/j1wyAIQ3mPAZbFji0vGcFo2Lp">Brain calc asciinema</a><br>
+<a href="https://asciinema.org/a/AAu12DdHgew9a6JBNRkDScuza">Brain gcd asciinema</a><br>
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { greeting, congratulations, answerCheck, correctMessage, incorrectMessage } from '../index.js';
+
+const gcd = (firstNumber, secondNumber) => {
+  if (secondNumber !== 0) {
+    const remainder = firstNumber % secondNumber;
+    return gcd(secondNumber, remainder);
+  }
+  return firstNumber;
+};
+
+const gcdGame = () => {
+  const userName = greeting();
+  let i = 0;
+  while (i < 3) {
+    const firstNumber = Math.floor(Math.random() * 100);
+    const secondNumber = Math.floor(Math.random() * 100);
+    const result = gcd(firstNumber, secondNumber);
+    console.log(`Find the greatest common divisor of given numbers.\n Question: ${firstNumber} ${secondNumber}`);
+    const userAnswer = readlineSync.questionInt('Your answer: ');
+    if (answerCheck(userAnswer, result)) {
+      correctMessage();
+      i += 1;
+    } else {
+      incorrectMessage(userAnswer, result, userName);
+      return;
+    }
+  }
+  congratulations(userName);
+};
+
+export default gcdGame;
